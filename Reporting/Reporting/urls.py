81db2041266ceaf3d7e265db6dafe7b948702fb5
dpadawer <daniel.padawer@gmail.com>
2014-03-29 12:37:10 -0400

    Have the (empty) fileupload page working

--- Reporting/Reporting/urls.py
+++ Reporting/Reporting/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from WebApp import views as webAppViews
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^upload', webAppViews.upload),
 )
