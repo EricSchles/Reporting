799ac3d1137776248c589e5e0054af5e14746ddd
Miklos Pataky <miklosp@gmail.com>
2014-03-29 12:17:18 -0400

    Adding models and ignoring certain files.

--- Reporting/WebApp/admin.py
+++ Reporting/WebApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from models import Website, Ad
 
 # Register your models here.
+admin.site.register(Website)
+admin.site.register(Ad)
--- Reporting/WebApp/models.py
+++ Reporting/WebApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Website(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Ad(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    ethnicity = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+    location = models.CharField(max_length=255)
+    ad = models.CharField(max_length=512)
+    date = models.DateTimeField()
+    website = models.ForeignKey(Website)
+
+    def __str__(self):
+        return self.name
