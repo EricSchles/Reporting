c7a5828ca9143092eb8bd719e2648642b6cc0060
dpadawer <daniel.padawer@gmail.com>
2014-03-29 17:25:23 -0400

    Playing with admin.py to allow filtering

--- Reporting/WebApp/admin.py
+++ Reporting/WebApp/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from WebApp.models import Website, Ad
 
+class WebsiteAdmin(admin.ModelAdmin):
+  fields = ['name', 'url']
+  list_filter = ['name', 'url']
+  
+class AdInline(admin.TabularInline):
+  model = Ad
+  
 # Register your models here.
-admin.site.register(Website)
+admin.site.register(Website, WebsiteAdmin)
+#admin.site.register(Ad, AdInline)
 admin.site.register(Ad)
