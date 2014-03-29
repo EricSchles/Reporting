3262a14c19d1bb97fd8d8b6045fb067ccba4e958
Miklos Pataky <miklosp@gmail.com>
2014-03-29 17:16:15 -0400

    Reorganizing urls.

--- Reporting/WebApp/urls.py
+++ Reporting/WebApp/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from WebApp import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.upload, name='upload'),
-  url(r'^confirm/$', views.confirm, name='confirm'),
-  url(r'^submit/$', views.success, name='Success'),
+  url(r'^upload/$', views.upload, name='upload'),
+  url(r'^upload/confirm/$', views.confirm, name='confirm'),
+  url(r'^upload/submit/$', views.success, name='Success'),
   url(r'^websites/(?P<website_id>[0-9]+)/?$', views.website_ads, name='Website Ads'),
   url(r'^websites/$', views.websites, name='Websites'),
 )
